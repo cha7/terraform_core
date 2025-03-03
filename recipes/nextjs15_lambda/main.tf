c3ec48340ffc57f8be940e08af5a0d2391731ffe
cha7 <root@DESKTOP-UDSKGK1.localdomain>
2025-03-02 16:34:00 -0800

    testing recipe structure

--- recipes/nextjs15_lambda/main.tf
+++ recipes/nextjs15_lambda/main.tf
@@ -5,41 +5,37 @@ variable "input_region" {
 
 provider "aws" {
   region = var.input_region
+  application_name = var.application_name
+  application_description = var.application_description
 }
 
 locals {
-  application_name                  = "test-app"
-  application_description           = "just a test"
-
   # Do not modify
   account_id                        = data.aws_caller_identity.current.account_id
   region                            = data.aws_region.current.name
 }
 
-
-
-
 module "lambda_function" {
     # Do not modify
     source                          = "git::https://github.com/cha7/terraform_core//modules/lambda_function"
-    lambda_function_name            = local.application_name
-    lambda_function_description     = local.application_description
+    lambda_function_name            = var.application_name
+    lambda_function_description     = var.application_description
     apigatewayv2_api_execution_arn  = module.api_gateway.apigatewayv2_api_execution_arn
 }
 
 module "api_gateway" {
     # Do not modify
     source                          = "git::https://github.com/cha7/terraform_core//modules/api_gateway"
+    api_gateway_name                = var.application_name
+    api_gateway_description         = var.application_description
     lambda_function_arn             = module.lambda_function.lambda_function_arn
-    api_gateway_name                = local.application_name
-    api_gateway_description         = local.application_description
     s3_bucket_website_endpoint      = module.s3_bucket.s3_bucket_website_endpoint
 }
 
 module "s3_bucket" {
     # Do not modify
     source                          = "git::https://github.com/cha7/terraform_core//modules/s3_bucket"
-    bucket_name                     = "${local.application_name}-${local.account_id}-${local.region}-static"
+    bucket_name                     = "${var.application_name}-${var.account_id}-${var.region}-static"
     api_id                          = module.api_gateway.api_id
 }
 
