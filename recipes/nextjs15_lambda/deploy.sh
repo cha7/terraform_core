229d578492297eb58e02e99610d33ede69f93fc8
cha7 <chan.jernigan@gmail.com>
2025-03-16 16:34:08 -0700

    Testing more stuff

--- recipes/nextjs15_lambda/deploy.sh
+++ recipes/nextjs15_lambda/deploy.sh
@@ -3,6 +3,8 @@
 
 source ./.env
 
+account-id=$(aws sts get-caller-identity --query Account --output text)
+
 echo "Found REGION: ${REGION}"
 echo "Found APP_NAME: ${APP_NAME}"
 echo "Found APP_DESCRIPTION: ${APP_DESCRIPTION}"
@@ -13,5 +15,12 @@ cp -r node_modules/terraform_core/modules ./infra
 cp -r node_modules/terraform_core/recipes/nextjs15_lambda/* ./infra
 
 cd infra
-terraform init
-terraform apply -var input_region=${REGION} -var input_application_name=${APP_NAME} -var input_application_description=${APP_DESCRIPTION} --auto-approve
+
+terraform init \
+-backend-config="bucket=terraform-${account-id}-us-east-1" \
+-backend-config="key=${APP_NAME}/${REGION}" \
+-backend-config="region=${REGION}"
+
+terraform apply -var input_region=${REGION} \
+-var input_application_name=${APP_NAME} \
+-var input_application_description=${APP_DESCRIPTION} --auto-approve
