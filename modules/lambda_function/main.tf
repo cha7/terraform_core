5eec7788649da88d432119db1baf3d9aa1b58b3d
cha7 <chan.jernigan@gmail.com>
2025-03-17 01:23:04 -0700

    Testing more stuff

--- modules/lambda_function/main.tf
+++ modules/lambda_function/main.tf
@@ -1,8 +1,8 @@
 locals {
   account_id = data.aws_caller_identity.current.account_id
   region = data.aws_region.current.name
-  security_group_list = data.aws_ssm_parameter.security_group_list.value
-  subnet_list = data.aws_ssm_parameter.subnet_list.value
+  security_group_list = split(",", data.aws_ssm_parameter.security_group_list.value)
+  subnet_list =  split(",", data.aws_ssm_parameter.subnet_list.value)
 
 }
 
@@ -18,8 +18,8 @@ module "lambda_function" {
   publish       = true
   layers        = ["arn:aws:lambda:${local.region}:753240598075:layer:LambdaAdapterLayerX86:23"]
 
-  vpc_security_group_ids = split(",", local.security_group_list)
-  vpc_subnet_ids         = split(",", local.subnet_list)
+  vpc_security_group_ids = ["${local.security_group_list}"]
+  vpc_subnet_ids         = ["${local.subnet_list}"]
   attach_network_policy = true
   
   environment_variables = {
